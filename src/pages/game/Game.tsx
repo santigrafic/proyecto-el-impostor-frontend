90dccda35df6b18462ce146439c49795c98cc59e
davidSantiagoValmesa <david.santiago@valmesa.com>
2026-05-07 14:10:49 +0200

    fix(project): arreglado game

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import VotingPhase from "./components/voting-phase/VotingPhase";
