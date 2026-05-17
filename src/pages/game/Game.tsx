da9c235d3561bbc205b55cec7d157efc3e0ab48b
David Santiago Gavilan <santigrafic@gmail.com>
2026-05-17 11:32:50 +0200

    fix(project): Arreglado error en game

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -319,37 +319,6 @@ useEffect(() => {
     }
   };
 
-  const handleExitGame = async () => {
-    const confirmExit = window.confirm(
-      "¿Seguro que quieres salir? La partida termina aquí.",
-    );
-
-    if (!confirmExit) return;
-
-    try {
-      const socketId = (window as any).Echo?.socketId();
-      const res = await fetch(`${API_URL}/api/games/${roomId}/exit`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "X-Socket-Id": socketId,
-        },
-        body: JSON.stringify({
-          roomId,
-          playerId,
-        }),
-      });
-
-      const data = await res.json();
-
-      console.log("EXIT RESPONSE:", data);
-
-      navigate("/home");
-    } catch (error) {
-      console.error("Error exiting game:", error);
-    }
-  };
-
   /*const finishGame = async () => {
     if (!gameId) return;
     try {
