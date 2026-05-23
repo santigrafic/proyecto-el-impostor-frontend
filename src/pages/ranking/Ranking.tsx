9237f80e2f729c4ba176b133f06c7e4e76b5c0c9
David Santiago Gavilan <santigrafic@gmail.com>
2026-05-23 11:52:10 +0200

    feat(project): Actualizado Ranking

--- src/pages/ranking/Ranking.tsx
+++ src/pages/ranking/Ranking.tsx
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from "react";
 import LoadingScreen from "../../commons/components/loadingScreen/LoadingScreen";
 
+import type { PlayerRanking } from "./utils/interfaces";
+
 import "./Ranking.css";
 
 const API_URL = import.meta.env.VITE_API_URL;
-
-interface PlayerRanking {
-  id: number;
-  nickname: string;
-  games_won: number;
-}
 
 const Ranking: React.FC = () => {
   const [ranking, setRanking] = useState<PlayerRanking[]>([]);
@@ -28,7 +24,7 @@ const Ranking: React.FC = () => {
       if (!res.ok) throw new Error("Error cargando ranking");
 
       const data = await res.json();
-      setRanking(data);
+      setRanking(data.data);
     } catch (err) {
       console.error(err);
     } finally {
@@ -44,7 +40,13 @@ const Ranking: React.FC = () => {
   return (
     <div className="ranking-container">
       <h1 className="ranking-title">TOP 16 JUGADORES</h1>
-
+      <section>
+        <li className="ranking-item-title">
+          <span className="ranking-left">Nickname</span>
+          <span className="ranking-item-title-space"></span>
+          <span className="ranking-right">WINS//%</span>
+        </li>
+      </section>
       <section className="ranking-section">
         {ranking.length === 0 ? (
           <p>No hay datos de ranking</p>
@@ -56,10 +58,8 @@ const Ranking: React.FC = () => {
                   <span className="cursor">&gt;</span>
                   {user.nickname}
                 </span>
-
                 <span className="ranking-dots"></span>
-
-                <span className="ranking-right">{user.games_won}</span>
+                <span className="ranking-right">{user.games_won}//{user.win_rate}%</span>
               </li>
             ))}
           </ol>
